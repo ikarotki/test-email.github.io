b9a3cd0ff38975ae975aea2f1803db10ba276b60
Ihar Karotki <igor.k@joblio.co>
2023-02-09 14:52:33 +0300

    the site is "now" live

--- api/paperboy.js
+++
@@ -1,6 +0,0 @@
-module.exports = (request, response) => {
-  if (request.method === 'POST')
-    response.status(200).send('OK');
-  else
-    response.status(400).send('Bad Request');
-};
---
+++ public/api/paperboy.js
@@ -0,0 +1,20 @@
+// use the .env if present, do nothing otherwise
+require('dotenv').config();
+// the utility to handle StaticEmail requests
+const {create} = require('static.email.ses');
+// secrets revealed via dotenv or now
+const {
+  AWS_SES_TO: to,
+  AWS_SES_REGION: region,
+  AWS_SES_ACCESS_KEY: accessKeyId,
+  AWS_SES_SECRET_KEY: secretAccessKey
+} = process.env;
+// the serverless function that now will use
+module.exports = create({
+  site: 'weirdsite.online',  // put your domain here
+  sender: 'Ihar Karotki',// any sender name
+  to,
+  region,
+  accessKeyId,
+  secretAccessKey
+});
